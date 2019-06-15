306d152ce2ae0090788aa2349cc9d6bb3305bab2
mr.wu <123@sina.com>
2019-06-15 08:10:09 +0800

    two

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import {Switch,BrowserRouter,Route} from 'react-router-dom';
+import {Login} from './pages/login/Login'
+import {Admin} from './pages/admin/Admin'
+
+
+export default class App extends Component{
+    render(){
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' component={Login}/>
+                    <Route path='/admin' component={Admin}/>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+}
